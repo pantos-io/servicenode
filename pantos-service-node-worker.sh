c05606164caa4dd8d99d2cf3560b09ff040fb133
Juan M. Tirado <juan.tirado@bitpanda.com>
2025-02-05 15:36:01 +0100

    fix: add celery ping

--- pantos-service-node-worker.sh
+++ pantos-service-node-worker.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-poetry run celery -A pantos.servicenode worker -l INFO -n pantos.servicenode -Q transfers,bids
+poetry run celery --config -A pantos.servicenode worker -l INFO -n pantos.servicenode -Q transfers,bids
